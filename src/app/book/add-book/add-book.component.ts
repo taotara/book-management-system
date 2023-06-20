451073ea44d810bb0936921e37e02aa38221ba2b
taotara <taotara@yahoo.com>
2023-06-20 06:23:40 +0100

    first commit

---
+++ src/app/book/add-book/add-book.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-add-book',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './add-book.component.html',
+  styleUrls: ['./add-book.component.css']
+})
+export class AddBookComponent {
+
+}
